631b49e3b3dea8e34e2130f7fcc9dffbcfc35ea6
Sagar <sagar.salukhe@obicas.in>
2023-02-06 12:26:57 +0530

    assign boothPart component

--- FrontEnd/src/app/user-module/assign-data/assign-data.component.ts
+++ FrontEnd/src/app/user-module/assign-data/assign-data.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import * as xlsx from 'xlsx';
 import { UserService } from 'src/app/services/user.service'
-import { ActivatedRoute } from '@angular/router'
+import { ActivatedRoute, Router } from '@angular/router'
+import { LoaderService } from 'src/app/services/loader.service'
+import { IonicToastService } from 'src/app/services/ionic-toast.service'
 
 @Component({
   selector: 'app-assign-data',
@@ -11,95 +13,104 @@ import { ActivatedRoute } from '@angular/router'
 export class AssignDataComponent implements OnInit {
 
   @ViewChild('epltable', { static: false }) epltable: ElementRef;
-  assignPart: any ={};
+  assignPart: any = {};
   Userid: any;
-  allBooths: any=[];
-  SelectedBooth:any=[];
-  UserpartNoAssigned :any; 
-  UserpartNoAssignedarray:[];
-  SelectedPartNo=[];
+  allBooths: any[] = [];
+  SelectedBooth: any = [];
+  UserpartNoAssigned: any;
+  UserpartNoAssignedarray: [];
+  SelectedPartNo = [];
   Uid: any;
-  id:number;
+  id: number;
 
   constructor
     (
       private user: UserService,
-      private route:ActivatedRoute
-    ){}
-  
+      private route: ActivatedRoute,
+      private toast: IonicToastService,
+      private loader: LoaderService,
+      private router: Router
+    ) { }
+
   ngOnInit() {
-    this.SelectedPartNo=[];
+    this.SelectedPartNo = [];
     this.user.getBooths().subscribe(data => {
       //console.log(data);
       this.allBooths = data;
-      this.allBooths.reduce((acc, allboothitem) => 
-      {
+      this.allBooths.reduce((acc, allboothitem) => {
         debugger;
-        for(let item in this.UserpartNoAssignedarray)
-        {
-            if(allboothitem.partNo==Number(this.UserpartNoAssignedarray[item])){
-              allboothitem.checked=true;
-              this.SelectedPartNo.push(allboothitem.partNo);
-              //item.checked=true;
-            }
-      }
-    }, []);
+        for (let item in this.UserpartNoAssignedarray) {
+          if (allboothitem.partNo == Number(this.UserpartNoAssignedarray[item])) {
+            allboothitem.checked = true;
+            this.SelectedPartNo.push(allboothitem.partNo);
+            //item.checked=true;
+          }
+        }
+      }, []);
     });
     debugger;
     this.Userid = this.route.snapshot.paramMap.get('id');
-    this.UserpartNoAssigned=this.route.snapshot.paramMap.get('partNoAssigned');
-    this.UserpartNoAssignedarray=this.UserpartNoAssigned.split(',');
+    this.UserpartNoAssigned = this.route.snapshot.paramMap.get('partNoAssigned');
+    this.UserpartNoAssignedarray = this.UserpartNoAssigned.split(',');
     //for(let allBooths of this.allBooths )
-    
+
     //this.UserpartNoAssigned.reduce(item)
-   // this.SelectedPartNo=
+    // this.SelectedPartNo=
     //console.log(this.Userid);
-   // this.assign();
+    // this.assign();
   }
 
-  getBoothPart(event:any) {
+  getBoothPart(event: any) {
     debugger;
     this.SelectedBooth = [];
-   // this.SelectedPartNo=[];
+    // this.SelectedPartNo=[];
     debugger;
-     if(event.target.checked){
-       var a=Number(event.target.value);
-       this.SelectedPartNo.push(a);
-     }
-     else {
-      const index=this.SelectedPartNo.indexOf(Number(event.target.value));
-      if(index > -1)
-      {
-        this.SelectedPartNo.splice(index,1);
+    if (event.target.checked) {
+      var a = Number(event.target.value);
+      this.SelectedPartNo.push(a);
+    }
+    else {
+      const index = this.SelectedPartNo.indexOf(Number(event.target.value));
+      if (index > -1) {
+        this.SelectedPartNo.splice(index, 1);
       }
-     }
+    }
     // let checkedStrings = this.allBooths.reduce((acc, item) => {
     //   if (item.checked) {
     //     this.SelectedPartNo.push(item.partNo);
     //   }
-      
+
     // }, []);
-    
-    
+
+
     console.log("data", this.SelectedPartNo.toString());
     console.log(this.SelectedPartNo);
   }
 
 
- 
+
   assign() {
-   this.id=Number(this.Userid);
-      this.user.assignPart({
-        UserId:this.id,
-        PartNoAssigned:this.SelectedPartNo.toString()
-      }).subscribe(data=>{
-        if(data==1){
-          console.log("sucess");
-          //redirect to the User page 
-          //shows the alert 
-          //edit the response from api --string like "Record added succesfully " -
-        }
+    this.loader.showLoading();
+    this.id = Number(this.Userid);
+    this.user.assignPart({
+      UserId: this.id,
+      PartNoAssigned: this.SelectedPartNo.toString()
+    }).subscribe(data => {
+      if (data == 1) {
+        this.loader.hideLoader();
+        console.log("sucess");
+        this.router.navigate(['/user']);
+        this.toast.presentToast("Part no. assigned successfully!", "success", 'checkmark-circle-sharp');
+        //edit the response from api --string like "Record added succesfully "
+      }
+      else {
+        this.loader.hideLoader();
+        this.toast.presentToast("Part no. not assigned", "danger", 'alert-circle-sharp');
+      }
       console.log(data);
+    }, (err) => {
+      this.loader.hideLoader();
+      this.toast.presentToast("User not saved", "danger", 'alert-circle-sharp');
     })
   }
 
@@ -107,7 +118,7 @@ export class AssignDataComponent implements OnInit {
   exportexcel() {
     const ws: xlsx.WorkSheet =
       xlsx.utils.table_to_sheet(this.epltable.nativeElement);
-     const wb: xlsx.WorkBook = xlsx.utils.book_new();
+    const wb: xlsx.WorkBook = xlsx.utils.book_new();
     xlsx.utils.book_append_sheet(wb, ws, 'Sheet1');
     xlsx.writeFile(wb, 'users.xlsx');
   }
